88fe8fe116080e3990a640d0c1db46d74c876833
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-11-15 23:00:00 +0000

    Server and db configuration

--- client/src/ProjectDetail.js
+++ client/src/ProjectDetail.js
@@ -10,7 +10,7 @@ export const ProjectDetail = [
     title: "Debuggers-Final-Project",
     description:
       "The project is a language exchange program website that allows student users to connect with language buddies to develop their language skills. It also allows admin users to create, edit, and delete upcoming events. The project uses React, Node.js, Express, and PostgreSQL.",
-    image: Image1,
+    image: "/images/screenshot-1.png",
     demo: "https://starter-kit-pzcq.onrender.com/",
     repo: "https://github.com/Dawit-Dev/Debuggers-Final-Project",
   },
@@ -19,7 +19,7 @@ export const ProjectDetail = [
     title: "Full Stack Project Assessment",
     description:
       "This is a web application that allows users to save, search, delete, and rate YouTube videos to watch later. Users can search for videos by title, delete saved videos, and rate them. The project uses React for the front-end, Node.js and Express for the back-end, and PostgreSQL for the database.",
-    image: Image2,
+    image: "/images/screenshot-2.png",
     demo: "https://dawit-youtube-video.onrender.com",
     repo: "https://github.com/Dawit-Dev/Full-Stack-Project-Assessment",
   },
@@ -28,7 +28,7 @@ export const ProjectDetail = [
     title: "TV Show Project",
     description:
       "This dynamic web app, inspired by Netflix, features an engaging UI for TV show and episode management. It relies on TVMaze.com for data, honing my JavaScript DOM manipulation skills vital in Frontend Web Dev. The tech stack includes HTML, CSS, JavaScript, and Bootstrap, enabling key features like search filters, select options, and seamless navigation between shows and episodes based on user choices.",
-    image: Image5,
+    image: "/images/Screenshot-6.png",
     demo: "https://cyf-dawit-dev-tv.netlify.app/",
     repo: "https://github.com/Dawit-Dev/tv-show-dom-project",
   },
@@ -37,7 +37,7 @@ export const ProjectDetail = [
     title: "Food-order-app",
     description:
       "This is a web application that enables users to place food orders and calculates the total cost. The project is built using React and for styling Css.",
-    image: Image4,
+    image: "/images/screenshot-4.png",
     demo: "https://dawit-food-order.netlify.app/",
     repo: "https://github.com/Dawit-Dev/food-order-app",
   },
--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-const app = require("express");
+const express = require("express");
+const prisma = require("../db/prisma");
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", async (req, res) => {
+    const projects = await prisma.project.findMany()
+    res.json(projects)
+})
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
